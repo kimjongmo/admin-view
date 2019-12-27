3185e1b90f1203e9867bdbfb3137e0443e2cd1bf
kimjongmo <kfmd1008@naver.com>
2019-12-27 16:08:16 +0900

    카테고리 공통화

---
+++ src/components/common/ContentHeader.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="content-header">
+        <h1>
+            {{tabList[0].title}}<!--{{header.title}}-->
+            <small>{{selected().title}}</small><!--{{header.currentTab}}-->
+        </h1>
+        <ol class="breadcrumb">
+            <li v-for="tab in tabList" :key="tab.title" :class="{active: tab.selected}">
+                <a @click="moveTab(tab.name,tab.href)">
+                    <i class="fa fa-dashboard"></i>{{tab.title}}
+                </a>
+            </li>
+        </ol>
+    </section>
+</template>
+
+<script>
+    export default {
+        props: ['tabList'],
+        name: "ContentHeader",
+        methods: {
+            selected: function () {
+                if(typeof this.tabList === 'object' && this.tabList !==''){
+                    return this.tabList.filter(tab => tab.selected)
+                }
+                return '';
+            },
+            moveTab: function (name,id) {
+                if(name!=='' && id!==''){
+                    this.$router.push({name: name, params: {id: id}});
+                }else if(name!=='' && id===''){
+                    this.$router.push({name: name});
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
