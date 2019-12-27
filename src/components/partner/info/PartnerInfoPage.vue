0de4ded8ec1ee5eb61d33680e3085a358ec8540a
kimjongmo <kfmd1008@naver.com>
2019-12-27 16:09:01 +0900

    파트너 정보에서 아이템리스트 보여주기

--- src/components/partner/info/PartnerInfoPage.vue
+++ src/components/partner/info/PartnerInfoPage.vue
@@ -1,16 +1,7 @@
 <template>
     <div class="content-wrapper">
         <!-- HEADER START-->
-        <section class="content-header">
-            <h1>
-                파트너사 관리
-                <small>파트너 정보</small>
-            </h1>
-            <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i>파트너사 관리</a></li>
-                <li class="active">파트너 정보</li>
-            </ol>
-        </section>
+        <content-header :tab-list="tabList"/>
         <!-- HEADER END-->
 
         <section class="content">
@@ -57,16 +48,60 @@
                 </table>
             </div>
         </section>
+
+        <!--############################################################################################ -->
+        <!--############################################################################################ -->
+        <!--##############################     ITEM_LIST    START    ################################### -->
+        <!--############################################################################################ -->
+        <!--############################################################################################ -->
+        <section class="content-header">
+            <h1>
+                판매 상품 리스트
+            </h1>
+        </section>
+        <hr>
+        <div class="row">
+            <div class="col-md-4" v-for="item in itemList" :key="item.id">
+                <figure class="card card-product">
+                    <div class="img-wrap"><img
+                            src="https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile10.uf.tistory.com%2Fimage%2F99ADC3485E000F8E2C71B9">
+                    </div>
+                    <figcaption class="info-wrap">
+                        <h4 class="title">{{item.name}}</h4>
+                        <p class="desc">{{item.title}}</p>
+                        <div class="price-wrap h3">
+                            <span class="price-new">{{numberFormat(item.price)}}원</span>
+                            <a @click="$router.push({name: 'itemInfo',params: {id: item.id}})"
+                               class="btn btn-sm btn-primary float-right">상세보기</a>
+                            <!--                            <del class="price-old">{{item.price}}</del>-->
+                        </div> <!-- price-wrap.// -->
+                    </figcaption>
+                </figure>
+            </div> <!-- col // -->
+        </div> <!-- row.// -->
+        <!--############################################################################################ -->
+        <!--############################################################################################ -->
+        <!--##############################      ITEM_LIST    END     ################################### -->
+        <!--############################################################################################ -->
+        <!--############################################################################################ -->
     </div>
 </template>
 
 <script>
     import {eventBus} from "../../../main";
+    import ContentHeader from "../../common/ContentHeader";
+
     export default {
         name: "PartnerInfoPage",
         data: function () {
             return {
-                partner: ''
+                partner: '',
+                itemList: '',
+                tabList: [
+                    {title: '파트너 관리', name: '', href: '', selected: false},
+                    {title: '파트너 리스트', name: 'partners', href: '', selected: false},
+                    {title: '파트너 정보', name: '', href: '', selected: true}
+                ]
             }
         },
         methods: {
@@ -74,6 +109,13 @@
                 if (typeof date === 'string')
                     return date.replace('T', ' ');
                 return date;
+            },
+            numberFormat: function (number) {
+                if (this == 0) return 0;
+                var reg = /(^[+-]?\d+)(\d{3})/;
+                var n = (number + '');
+                while (reg.test(n)) n = n.replace(reg, '$1' + ',' + '$2');
+                return n;
             }
         },
         created() {
@@ -84,16 +126,55 @@
                         alert('데이터가 존재하지 않습니다.');
                     } else {
                         this.partner = result.data.data;
+                        this.itemList = result.data.data.item_list;
                     }
                 }).catch();
 
 
             /* 카테고리 선택 활성화*/
-            eventBus.$emit('setSelectedName','partners');
+            eventBus.$emit('setSelectedName', 'partners');
+        },
+        components: {
+            ContentHeader
         }
     }
 </script>
 
 <style scoped>
+    .card-product .img-wrap {
+        border-radius: 3px 3px 0 0;
+        overflow: hidden;
+        position: relative;
+        height: 220px;
+        text-align: center;
+    }
 
+    .card-product .img-wrap img {
+        max-height: 100%;
+        max-width: 100%;
+        object-fit: cover;
+    }
+
+    .card-product .info-wrap {
+        overflow: hidden;
+        padding: 15px;
+        border-top: 1px solid #eee;
+    }
+
+    .card-product .bottom-wrap {
+        padding: 0px 15px 15px 15px;
+        border-top: 1px solid #eee;
+    }
+
+    .card-product .price-wrap > a {
+        position: absolute;
+        right: 28px;
+        bottom: 15px;
+    }
+
+    .table-bordered,
+    .table-bordered > thead > tr > th ,
+    .table-bordered > thead > tr > td {
+        border: 1px solid #8c8c8c;
+    }
 </style>
